16df1c3f97bfcddf4dc0b2c66968c324e15fea7c
krishb23535 <87112515+krishb23535@users.noreply.github.com>
2021-10-17 12:49:24 +0530

    Update client.js

--- js/client.js
+++ js/client.js
@@ -22,7 +22,7 @@ const append = (message, position)=>{
 
 
 // Ask new user for his/her name and let the server know
-const name = prompt("Enter your name to join");
+var name = window.prompt("Would you give us your name please?");
 socket.emit('new-user-joined', name);
 
 // If a new user joins, receive his/her name from the server
